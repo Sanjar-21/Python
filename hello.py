aaa07894ade832c2a888ba84541cedfc7519aa40
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-11 19:02:53 +0500

    Python da printda amalar

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 # Ananaga sodiq qolgan holda boshlaymiz python3 ni
-print("Hello World!\n")
+print("Hello World!\n") # pythonda ham c tiliga uhshab pastki qatorga new line \n day yozilda pythonda ham 
 print("Salom Dunyo")
+# printda faqat suzlarni mas amalarni ham choq qilsak buladi ekranga
+print(42 // 2)
+print("Uzbekiston\n") # print kiyinga dasrslarda mukamal darajada utamiz 
 # print suzi ekranga chop qilish konsilga
