52cadc92b6b37b25a85c4944d1f87abeed0119b2
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-11 22:57:37 +0500

    Print(), arifmetik amallar va sinteks

--- hello.py
+++ hello.py
@@ -3,5 +3,19 @@ print("Hello World!\n") # pythonda ham c tiliga uhshab pastki qatorga new line \
 print("Salom Dunyo")
 # printda faqat suzlarni mas amalarni ham choq qilsak buladi ekranga
 print(42 // 2)
-print("Uzbekiston\n") # print kiyinga dasrslarda mukamal darajada utamiz 
+print("Uzbekiston\n") # print kiyinga dasrslarda mukamal darajada utamiz   
 # print suzi ekranga chop qilish konsilga
+print('Hello World!') # python da '' va "" ichida yozsag ham buladi
+print('Mel \"DEL\" markasidagi noutbook sotib oldim\n') # \"  \" shunday chuqrsak ham buldi.
+# matini ikki qatorga tushirish uchun yanna yuli bor
+print("""adfadsfasdfasdfasdfadsf,
+      fasdadsfadfadsfasdfadsfadf
+      afasdfadsfasdfadsfasdfasdfads
+      fasdfasdfadsfasdfasdfasdfasdfads""")
+
+print("O\'zbekiston yo\'q \n")
+"""
+pthonda  arfmetik amalar
+
+""" 
+print(2*2/25)
