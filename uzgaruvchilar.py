929e576ba509aa6ee3c48ffa245c4642124431e9
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-12 11:29:51 +0500

    Python da RAM bilan ishlash

--- uzgaruvchilar.py
+++ uzgaruvchilar.py
@@ -1,6 +1,15 @@
 # 12.12.2024
 # Python 3.12.7 versiyasi
-
+# Python uzgaruvchilar c tilidan farqli uraloq
+# hotiradan joy olmaydi tug'ridan tug'ri
+# python da RAM joy oladi?
+import sys
 yosh = 20
+number = 23242323232342423
 ism = "Sanjar"
 print(f'Ism {ism} \n Yosh {yosh}')
+# pythonda RAM da turgan joyi uzgaruvchilarni ?
+print(sys.getsizeof(yosh))
+print(sys.getsizeof(ism))
+print(" Number: ", sys.getsizeof(number))
+print(sys.platform)#sestime haqida habar beradi ?
