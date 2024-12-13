70df147b54d396540667f35ef974c243f5b19019
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-13 12:26:11 +0500

    list python da

---
+++ list.py
@@ -0,0 +1,13 @@
+import sys
+# # 13.12.2024 # #
+# # bugingi dars list # #
+# # list belgisi [ ] ichiga yoziladi # #
+mevalar = ['olam', 'gilos', 'anjir', 'hurmo'] 
+print("mevelar size: " , sys.getsizeof(mevalar))
+print(hex(id(mevalar)), "hotiradigi manzili") # hotiradan manzili
+print(mevalar[0])
+print(mevalar[1])
+print(mevalar[2])
+print(mevalar[3])
+print(mevalar)
+
