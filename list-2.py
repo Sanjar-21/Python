cdf46d8b0838a892cb04e7f42bf7b61f91816b2e
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-16 00:34:47 +0500

    Listlar bilan ishlash ikkinchi kun

---
+++ list-2.py
@@ -0,0 +1,15 @@
+print("Listlar bilan ishlaymiz")
+
+mevalar = [
+    'olma',
+    'gilos',
+    'urik',
+    'shatoli1'
+]
+# # sorted bu ruyatga tegmasdan tartib lab chiqaradi listni 
+
+print(sorted(mevalar))
+
+## agar listni tatiblash uchun 
+mevalar.sort()
+print(f" mevalar {mevalar}")
