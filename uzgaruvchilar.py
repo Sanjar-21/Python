dae78b3a768db89447d79e14c415fd0a9f004504
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-17 21:41:17 +0500

    uzgaruchilae

--- uzgaruvchilar.py
+++ uzgaruvchilar.py
@@ -4,15 +4,16 @@
 # hotiradan joy olmaydi tug'ridan tug'ri
 # python da RAM joy oladi?
 import sys
+
 yosh = 20
 number = 23242323232342423
 ism = "Sanjar"
 s = 21.21
-print(f'Ism {ism} \n Yosh {yosh}')
+print(f"Ism {ism} \n Yosh {yosh}")
 # pythonda RAM da turgan joyi uzgaruvchilarni ?
 print("Yoshni sizoefi", sys.getsizeof(yosh))
 print("Ismni sizeofi", sys.getsizeof(ism))
 print(" Number: ", sys.getsizeof(number))
-print(sys.platform)#sestime haqida habar beradi ?
-print("Flaotni sizeof",sys.getsizeof(s))
+print(sys.platform)  # sestime haqida habar beradi ?
+print("Flaotni sizeof", sys.getsizeof(s))
 print("Xotira manzili", hex(id(yosh)))
