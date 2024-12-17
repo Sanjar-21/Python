30f5b456c0f480047920d5e3baff66ea6dfc332e
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-17 21:40:38 +0500

    Hello Wolrd! Salom Dunyo? print

--- hello.py
+++ hello.py
@@ -1,21 +1,27 @@
 # Ananaga sodiq qolgan holda boshlaymiz python3 ni
-print("Hello World!\n") # pythonda ham c tiliga uhshab pastki qatorga new line \n day yozilda pythonda ham 
+print(
+    "Hello World!\n"
+)  # pythonda ham c tiliga uhshab pastki qatorga new line \n day yozilda pythonda ham
 print("Salom Dunyo")
 # printda faqat suzlarni mas amalarni ham choq qilsak buladi ekranga
 print(42 // 2)
-print("Uzbekiston\n") # print kiyinga dasrslarda mukamal darajada utamiz   
+print("Uzbekiston\n")  # print kiyinga dasrslarda mukamal darajada utamiz
 # print suzi ekranga chop qilish konsilga
-print('Hello World!') # python da '' va "" ichida yozsag ham buladi
-print('Mel \"DEL\" markasidagi noutbook sotib oldim\n') # \"  \" shunday chuqrsak ham buldi.
+print("Hello World!")  # python da '' va "" ichida yozsag ham buladi
+print(
+    'Mel "DEL" markasidagi noutbook sotib oldim\n'
+)  # \"  \" shunday chuqrsak ham buldi.
 # matini ikki qatorga tushirish uchun yanna yuli bor
-print("""adfadsfasdfasdfasdfadsf,
+print(
+    """adfadsfasdfasdfasdfadsf,
       fasdadsfadfadsfasdfadsfadf
       afasdfadsfasdfadsfasdfasdfads
-      fasdfasdfadsfasdfasdfasdfasdfads""")
+      fasdfasdfadsfasdfasdfasdfasdfads"""
+)
 
-print("O\'zbekiston yo\'q \n")
+print("O'zbekiston yo'q \n")
 """
 pthonda  arfmetik amalar
 
-""" 
-print(2*2/25)
+"""
+print(2 * 2 / 25)
