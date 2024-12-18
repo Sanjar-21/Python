54aebcb42fecadd57367a4b051130512b7cefffe
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-18 18:11:53 +0500

    list

--- list-2.py
+++ list-2.py
@@ -1,18 +1,13 @@
 print("Listlar bilan ishlaymiz")
 
-mevalar = [
-    'olma',
-    'gilos',
-    'urik',
-    'shatoli1'
-]
-# # sorted bu ruyatga tegmasdan tartib lab chiqaradi listni 
+mevalar = ["olma", "gilos", "urik", "shatoli1"]
+# # sorted bu ruyatga tegmasdan tartib lab chiqaradi listni
 
 print(sorted(mevalar))
 # # ruyhatdan tegmasdan teskarisga chiqarish
-print(sorted(mevalar, reverse=True)) 
+print(sorted(mevalar, reverse=True))
 
-## agar listni tatiblash uchun 
+## agar listni tatiblash uchun
 mevalar.sort()
 print(f" mevalar {mevalar}")
 
@@ -20,16 +15,17 @@ mevalar.sort(reverse=True)
 
 print(f" mevalar {mevalar}")
 # #  range(funksiya) bu sonlar ruyhatni qaytaradi ?
-sonlar = list(range(1,11)) # 11  gacha bulgan sonlarni ruyhatini beradi ?
+sonlar = list(range(1, 11))  # 11  gacha bulgan sonlarni ruyhatini beradi ?
 print(sonlar)
 # # range bilan toq sonlar buladi
-toq = list(range(1,11,2))
+toq = list(range(1, 11, 2))
 print(f"toq sonalar {toq}")
 # # just sonlarga ham shunday buladi
-juft = list(range(0,10,2))
+juft = list(range(0, 10, 2))
 print(f"just sonlar {juft}")
-katta = max(juft) # max mim sonlar
-mayda = min(toq) # min sonlar 
+katta = max(juft)  # max mim sonlar
+mayda = min(toq)  # min sonlar
 print(f"\tmax {katta} \n\tmin {mayda}")
-num = sum(sonlar); print(num)
+num = sum(sonlar)
+print(num)
 print(sonlar[0:4])
