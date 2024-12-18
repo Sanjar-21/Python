2251cf8b2c18666292d933c20bc5902dcd3b4fb5
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-18 18:11:20 +0500

    list

--- list-2.py
+++ list-2.py
@@ -19,3 +19,17 @@ print(f" mevalar {mevalar}")
 mevalar.sort(reverse=True)
 
 print(f" mevalar {mevalar}")
+# #  range(funksiya) bu sonlar ruyhatni qaytaradi ?
+sonlar = list(range(1,11)) # 11  gacha bulgan sonlarni ruyhatini beradi ?
+print(sonlar)
+# # range bilan toq sonlar buladi
+toq = list(range(1,11,2))
+print(f"toq sonalar {toq}")
+# # just sonlarga ham shunday buladi
+juft = list(range(0,10,2))
+print(f"just sonlar {juft}")
+katta = max(juft) # max mim sonlar
+mayda = min(toq) # min sonlar 
+print(f"\tmax {katta} \n\tmin {mayda}")
+num = sum(sonlar); print(num)
+print(sonlar[0:4])
