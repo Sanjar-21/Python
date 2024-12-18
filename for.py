faed46d3b373cac6be762fbec6c21515bf8513cb
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-18 23:36:38 +0500

    python da for

--- for.py
+++ for.py
@@ -1,5 +1,25 @@
 # for sikli
 
-mehmonlar = ['hasan', 'ali', 'vali', 'malik', 'omon']
+mehmonlar = ["hasan", "ali", "vali", "malik", "omon"]
 for mehmon in mehmonlar:
     print(f"salom {mehmon}")
+
+# sonlar ustida ham for kuramiz
+
+sonlar = list(range(1, 11))
+for son in sonlar:
+    print(f"{son} sonlarni kvadrati {son**2} ga teng")
+
+nums = list(range(11))
+nums_kvadrati = []
+for num in nums:
+    nums_kvadrati.append(num**2)
+
+print(f"{nums_kvadrati}")
+
+dustlar = []
+print("Beshta eng yaqin dustigizni belgilag: ")
+for son in range(5):
+    dustlar.append(input(f"{son+1}-dustignizni kirtig ismini: "))
+
+print(dustlar)
