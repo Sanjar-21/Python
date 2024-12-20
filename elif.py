5c8f791cd7af1157a270a196db2920bd47cd549f
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-20 15:36:47 +0500

    elif shart aperatiri

---
+++ elif.py
@@ -0,0 +1,13 @@
+# # elif pythonda 
+son = int(input(f"son krtig: "))
+if son <= 4:
+    narh = 0
+elif son <= 12:
+    narh = 5
+elif son <= 25:
+    narh = 8
+elif son <= 35:
+    narh = 10
+else:
+    narh = 15
+print(f"\tSizga narh {narh} so\'m\n")
