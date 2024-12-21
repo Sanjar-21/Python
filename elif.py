2bb8bdb01bd4712d06d55745b5c3f78c545fe953
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-21 16:39:10 +0500

    elif pythonda

--- elif.py
+++ elif.py
@@ -1,16 +1,16 @@
-# # elif pythonda 
-# son = int(input(f"son krtig: "))
-# if son <= 4:
-#     narh = 0
-# elif son <= 12:
-#     narh = 5
-# elif son <= 25:
-#     narh = 8
-# elif son <= 35:
-#     narh = 10
-# else:
-#     narh = 15
-# print(f"\tSizga narh {narh} so\'m\n")
+# elif pythonda 
+son = int(input(f"son krtig: "))
+if son <= 4:
+    narh = 0
+elif son <= 12:
+    narh = 5
+elif son <= 25:
+    narh = 8
+elif son <= 35:
+    narh = 10
+else:
+    narh = 15
+print(f"\tSizga narh {narh} so\'m\n")
 
 kun = input("kuni krtig: ")
 if kun.lower() == 'shanba' or kun.lower() == 'yakshanba':
