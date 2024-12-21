f9806cbf419cef013eb09458ec262e148aab80a8
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-21 16:30:41 +0500

    elif or va and python

--- elif.py
+++ elif.py
@@ -1,13 +1,21 @@
 # # elif pythonda 
-son = int(input(f"son krtig: "))
-if son <= 4:
-    narh = 0
-elif son <= 12:
-    narh = 5
-elif son <= 25:
-    narh = 8
-elif son <= 35:
-    narh = 10
+# son = int(input(f"son krtig: "))
+# if son <= 4:
+#     narh = 0
+# elif son <= 12:
+#     narh = 5
+# elif son <= 25:
+#     narh = 8
+# elif son <= 35:
+#     narh = 10
+# else:
+#     narh = 15
+# print(f"\tSizga narh {narh} so\'m\n")
+
+kun = input("kuni krtig: ")
+if kun.lower() == 'shanba' or kun.lower() == 'yakshanba':
+    print("Bugin kun dam olish kuni\n")
 else:
-    narh = 15
-print(f"\tSizga narh {narh} so\'m\n")
+    print("Bugin ish kuni\n")
+
+
