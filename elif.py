386b953b7666311621eb36bcd605f621d91d89bc
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-21 16:47:19 +0500

    elif

--- elif.py
+++ elif.py
@@ -1,4 +1,4 @@
-# elif pythonda 
+# elif pythonda
 son = int(input(f"son krtig: "))
 if son <= 4:
     narh = 0
@@ -10,12 +10,10 @@ elif son <= 35:
     narh = 10
 else:
     narh = 15
-print(f"\tSizga narh {narh} so\'m\n")
+print(f"\tSizga narh {narh} so'm\n")
 
 kun = input("kuni krtig: ")
-if kun.lower() == 'shanba' or kun.lower() == 'yakshanba':
+if kun.lower() == "shanba" or kun.lower() == "yakshanba":
     print("Bugin kun dam olish kuni\n")
 else:
     print("Bugin ish kuni\n")
-
-
