6d9ab14625bc4be26b398be6abe5d2ea89c8fb5f
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-22 13:24:16 +0500

    Lug'at pythonda

--- lug'at.py
+++ lug'at.py
@@ -1,3 +1,22 @@
-# # Pyrhonda dikshinir
+# # Pyrhonda Lug'at
 # # 22 12 2024
 
+book = {"nomi": "python", "rangi": "sariq", "qalinligi": 1000}
+
+# print(f"kiton {book['nomi']}, rangi {book['rangi']}, qalinligi {book['qalinligi']}")
+
+talaba = {"ism": "sanjar", "yosh": 20, "yil": 2004}
+print(
+    f"Talaba ismi {talaba['ism'].title()},\
+      talaba yosh-{talaba['yosh']},\
+      tug'ilgan yili-{talaba['yil']}"
+)
+# # Lug'atga malumot kirtish
+talaba["kurs"] = 4
+print(talaba)
+
+# # get metidi shu lug'atda bormi yuqmi tekshiradi
+# #
+
+bir = talaba.get("isma", "Bunday suz mavjud emas")
+print(bir)
