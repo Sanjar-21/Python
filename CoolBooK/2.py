e5cbdbcf8273692b741a08fc7e384e3f72c76ac4
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-22 18:00:47 +0500

    heapq kata va kichik ni aniqlash

--- CoolBooK/2.py
+++ CoolBooK/2.py
@@ -10,8 +10,3 @@ print(heapq.nlargest(1,nums)) # Natija [42,37,23]
 # Eng kichik uchta elenet
 print(heapq.nsmallest(1, nums)) # Natija [-4,1,2]
 
-def sanjar(nums):
-    return print(heapq.nlargest(1,nums))
-
-ol = [1,8,2,23,7,-1,18,23,42,37,2]
-sanjar(ol)
