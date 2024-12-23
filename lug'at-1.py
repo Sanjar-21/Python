40a00ed60ba4197ebbdc00fef50c0fbbbeda5882
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-23 18:04:27 +0500

    lug'at ustida amalar bajarish

---
+++ lug'at-1.py
@@ -0,0 +1,11 @@
+# # Lug'at bilan ishlaymiz ikkinchi dars
+talaba = {
+    'ism':'Sanjar',
+    'familya':'Xolmuminov',
+    'fakultet':'IT',
+    'kurs':4
+}
+
+# print(f"talalar ruyati {talaba.items()}")
+for kalit, qiymat in talaba.items():
+    print(f"Kalit -->{kalit}\nQiymat -->{qiymat}")
