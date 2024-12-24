b02be2c7f9aa307145a5f524071fdbddd2048c6c
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-24 19:33:03 +0500

    OrderedDict cookbook

--- CooKBooK/5.py
+++ CooKBooK/5.py
@@ -1,9 +1,20 @@
 from collections import OrderedDict
+import json
 d = OrderedDict()
 d['foo'] = 1
 d['bar'] = 2
 d['spam'] = 3
 d['grok'] = 4
 
-for key, value in d.items():
-    print(key, d[key])
+# for key, value in d.items():
+#     print(key, d[key])
+
+b = OrderedDict()
+b['foo'] = 1
+b['bar'] = 2
+b['spam'] = 3
+b['grok'] = 4
+result = json.dumps(b)
+print(result)
+b['bar'] = 42
+print(result)
