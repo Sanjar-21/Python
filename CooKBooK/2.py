98748aa0784a65146baa40943741f1530c65c4e9
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-26 11:55:55 +0500

    listni enga katta elemntlarni topish

--- CooKBooK/2.py
+++ CooKBooK/2.py
@@ -5,8 +5,8 @@ nums = [1,8,2,23,7,-1,18,23,42,37,2]
 
 # Eng katta uchta elementni topish
 
-print(heapq.nlargest(1,nums)) # Natija [42,37,23]
+print(heapq.nlargest(3,nums)) # Natija [42,37,23]
 
 # Eng kichik uchta elenet
-print(heapq.nsmallest(1, nums)) # Natija [-4,1,2]
+print(heapq.nsmallest(3, nums)) # Natija [-4,1,2]
 
